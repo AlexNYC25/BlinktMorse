2f4fc293469f309cd4d25093d219ba49d3d62fe6
Alexis Montes <montes.alexis25@gmail.com>
2019-07-03 01:06:20 -0400

    moved tests into dic

--- MorseTest.py
+++
@@ -1,9 +0,0 @@
-
-# Test to use the MorseAlphabet object where it prints
-# on the terminal the Morse sequence 
-
-from MorseAlphabet import *
-
-tempAlph = MorseAlphabet()
-
-print(tempAlph.letterToMorse("A"))
